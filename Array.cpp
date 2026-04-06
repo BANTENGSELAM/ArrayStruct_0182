f342c9a3423bbd162abf4c36a3c0f31b6014d7f8
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-04-06 14:51:47 +0700

    tampilkan data

--- Array.cpp
+++ Array.cpp
@@ -9,4 +9,7 @@ int main() {
     int aku[5] = {10,20,30,40,50};
     //deklarasi tanpa inisialisasi
     int kamu[5];
+
+    //tampilkan data
+    cout << "Data Pada Index 1 = " << aku[1] << endl;
 }
