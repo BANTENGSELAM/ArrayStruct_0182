34707805881b2473ca9c7fce64ff0cdab7345703
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-04-06 15:24:27 +0700

    program isi struct

--- Struct.cpp
+++ Struct.cpp
@@ -8,3 +8,16 @@ struct Orang
     string alamat;
     int umur;
 };
+
+int main() {
+    Orang mhs;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamata : ";
+    cin >> mhs.alamat;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    
+}
