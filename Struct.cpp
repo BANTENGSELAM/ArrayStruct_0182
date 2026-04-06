21fa4baaf485da02eb44b52d1091cb94386f93a8
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-04-06 15:25:51 +0700

    menampilkan isi struct

--- Struct.cpp
+++ Struct.cpp
@@ -13,11 +13,14 @@ int main() {
     Orang mhs;
     cout << "Nama : ";
     cin >> mhs.nama;
-    cout << "Alamata : ";
+    cout << "Alamat : ";
     cin >> mhs.alamat;
     cout << "Umur : ";
     cin >> mhs.umur;
     cout << endl;
 
-    
+    // tampilkan   
+    cout << "Nama : " << mhs.nama << endl; 
+    cout << "Alamat : " << mhs.alamat << endl; 
+    cout << "Umur : " << mhs.umur << endl; 
 }
